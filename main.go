960c10200d1768844348b99a50f90c4ace8d49f0
Digvijay Kamble <kamble.digvijayn@gmail.com>
2024-12-20 11:37:49 +0530

    Add files via upload
    Hello World 
    program in go programming language

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+	fmt.Println("hi go")
+
+
+	var name string = "Digvijay"
+	fmt.Println(name)
+
+	var decided int = 30000
+	fmt.Println(decided)
+
+	var dimension float64 = 14.2
+	fmt.Println(dimension)
+	
+
+
+}
